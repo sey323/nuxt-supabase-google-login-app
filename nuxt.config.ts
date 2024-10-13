1ab165d7e49f94921daac1797ec7d22348d8940c
sey323 <sey323_nm@yahoo.co.jp>
2024-10-13 19:42:52 +0900

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,4 @@
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/supabase"],
+  compatibilityDate: "2024-10-13",
+});
---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const supabase = useSupabaseClient();
+
+  const signInWithGoogle = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/confirm",
+      },
+    });
+    if (error) console.log(error);
+  };
+</script>
+<template>
+  <div>
+    <button @click="signInWithGoogle">Sign In with Google</button>
+  </div>
+</template>
