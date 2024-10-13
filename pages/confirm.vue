8930572ab2b8c4847e293539efe7b82fd384074a
sey323 <sey323_nm@yahoo.co.jp>
2024-10-13 23:16:04 +0900

    リダイレクトパスのクッキー処理を削除し、ユーザーがログインした際にルートにリダイレクトするように変更

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
   const user = useSupabaseUser();
 
-  // Get redirect path from cookies
-  const cookieName = useRuntimeConfig().public.supabase.cookieName;
-  const redirectPath = useCookie(`${cookieName}-redirect-path`).value;
-
   watch(
     user,
     () => {
       if (user.value) {
-        // Clear cookie
-        useCookie(`${cookieName}-redirect-path`).value = null;
-        // Redirect to path
-        return navigateTo(redirectPath || "/");
+        return navigateTo("/");
       }
     },
     { immediate: true }
